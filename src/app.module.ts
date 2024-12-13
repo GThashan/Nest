a047653b9ef4bea122255755fbf0747aa0f45652
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-12-13 18:20:56 +0530

    todo moduel controller service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 
 @Module({
 
   controllers: [AppController],
   providers: [AppService],
+  imports: [TodoModule],
 })
 export class AppModule{}
