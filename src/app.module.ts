e30d7777a82f3b8fa0e26dc8886a3cb32311a909
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-12-13 18:37:38 +0530

    set up database in mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+
+const ormOptions : TypeOrmModuleOptions={
+  type:'mysql',
+  host:'localhost',
+  port:3306,
+  username:'root',
+  password:"",
+  database:'nestjs',
+  autoLoadEntities:true,
+  synchronize:true
+}
 
 @Module({
 
   controllers: [AppController],
   providers: [AppService],
-  imports: [TodoModule],
+  imports: [TodoModule, TypeOrmModule.forRoot(ormOptions)],
 })
 export class AppModule{}
